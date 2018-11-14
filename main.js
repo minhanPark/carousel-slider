2b19c3cee92ff5e071b40c2a211727f9cfefb610
minhanPark <alsgks1218@naver.com>
2018-11-14 16:41:07 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const first = document.querySelector('.slide');
+console.log(first);
+
+const slide = () => {
+  const before = document.querySelector('.showing');
+  if (before) {
+    before
+      .classList
+      .remove('showing');
+    const next = before.nextElementSibling;
+    if (next) {
+      next
+        .classList
+        .add('showing')
+    } else {
+      first
+        .classList
+        .add('showing');
+    }
+  } else {
+    first
+      .classList
+      .add('showing');
+  }
+}
+slide();
+setInterval(slide, 1500);
